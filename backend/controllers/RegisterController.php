bc800471cb2dd2d2ff4ffaa2b1eccaba075c5ab1
zhousong <song.zhou@maimob.cn>
2017-04-28 14:19:55 +0800

    Test

--- backend/controllers/RegisterController.php
+++ backend/controllers/RegisterController.php
@@ -4,10 +4,14 @@ namespace backend\controllers;
 use yii\base\Controller;
 use common\library\Utils;
 class RegisterController extends Controller{
+    public $layout = "register";
     public function actionTest(){
         $out= array(1,2,3,4,5,6);
         Utils::jsonOut($out);
     }
-    
+    public function actionIndex(){
+        echo 111;
+        $this->render('index');
+    }
     
 }
