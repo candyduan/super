9c03dd6a343a1cc81128def64d0d6c225feadf74
yuyan <yanyu@yanyudeMac-mini.lan>
2017-05-08 16:56:37 +0800

    Add campaign sdk goods  modify

--- common/models/orm/extend/CampaignSdkGoods.php
+++ common/models/orm/extend/CampaignSdkGoods.php
@@ -5,10 +5,33 @@ use common\library\Utils;
 
 class CampaignSdkGoods extends \common\models\orm\base\CampaignSdkGoods {
 
-  /*  public static function getGoodsByCaid($caid)
+
+    public static function getGoodsByCsid($csid)
     {
-        $data = self::find()->select(['id','name','fee'])->where(['deleted' => 0,'campaign' => $caid])->all();
-        return $data;
-    }*/
+        $data = self::find()->select(['goid','priceSign'])->where(['csid' => $csid])->all();
+        $return_data = [];
+        foreach($data as $value){
+            $return_data[$value['goid']] = $value['priceSign'];
+        }
+        return $return_data;
+    }
+
+    public static function getStatusByCsidGoid($csid,$goid)
+    {
+        $data = self::find()->select(['status'])->where(['csid' => $csid,'goid'=> $goid])->one();
+        return isset($data['status']) ? $data['status'] : '';
+    }
+
+
+    public static function deleteByCsidGoid($csid, $goid)
+    {
+        self::deleteAll(['csid' => $csid,'goid' => $goid]);
+    }
+
+    public static function findByCsidGoid($csid,$goid){
+
+        $model  = self::find()->where(['csid' => $csid,'goid' => $goid])->one();
+        return $model;
+    }
 
 }
