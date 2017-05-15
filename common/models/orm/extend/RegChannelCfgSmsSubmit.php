cac191e2f658680034190cedb3a38d94f68e1dfb
zhousong <song.zhou@maimob.cn>
2017-05-15 14:58:19 +0800

    /register/gcmf bugfix

--- common/models/orm/extend/RegChannelCfgSmsSubmit.php
+++ common/models/orm/extend/RegChannelCfgSmsSubmit.php
@@ -1,5 +1,19 @@
 <?php
 namespace common\models\orm\extend;
 class RegChannelCfgSmsSubmit extends \common\models\orm\base\RegChannelCfgSmsSubmit{
+    public static function findByRcid($rcid){
+        if(!is_numeric($rcid)){
+            return null;
+        }
+        $condition  = array(
+            'rcid'      => $rcid,
+            'status'    => 1,
+        );
+        $model  = self::find()
+                    ->where($condition)
+                    ->one()
+                    ;
+        return $model;
+    }
     
 }
