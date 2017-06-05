049debff60d3a1147d92e7635a70ffa2788e93a8
xushibing <shibing.xu@maimob.cn>
2017-06-05 14:03:21 +0800

    bugfix

--- common/models/orm/extend/CampaignPackage.php
+++ common/models/orm/extend/CampaignPackage.php
@@ -103,7 +103,7 @@ from campaignPackage as cp
 inner join partner as c on (cp.media = c.id)
 inner join partner as p on (cp.partner = p.id)
 where p.belong = 1 and p.id = $pid
-group by c.id");
+group by cp.mediaSign");
         return $command->queryAll();
     }
     
@@ -116,7 +116,7 @@ group by c.id");
             inner join partner as c on (cp.media = c.id)
             inner join partner as p on (cp.partner = p.id)
             where p.belong = 1 and cp.media = $media
-            group by c.id");
+            group by cp.mediaSign");
         return $command->queryAll();
     }
     
