79cd4882dfaa6247e72dafa19d688c7753d1fe80
zhousong <song.zhou@maimob.cn>
2017-07-20 16:49:30 +0800

    适配手机展示

--- backend/library/widgets/WidgetsUtils.php
+++ backend/library/widgets/WidgetsUtils.php
@@ -162,7 +162,6 @@ class WidgetsUtils{
         $str ='
             <meta charset="utf-8">
             <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1">
             <meta name="description" content="">
             <meta name="author" content="">
           
